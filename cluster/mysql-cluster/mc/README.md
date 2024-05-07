f7857fc189823f2edcc67ea600666f50b46ec079
xulei <373045134@qq.com>
2024-05-07 14:56:01 +0800

    脚本自动push

--- cluster/mysql-cluster/mc/README.md
+++ cluster/mysql-cluster/mc/README.md
@@ -25,7 +25,6 @@
 SELECT
 	COLUMN_NAME 列名,
 	COLUMN_TYPE 数据类型,
-	DATA_TYPE 字段类型,
 	CHARACTER_MAXIMUM_LENGTH 长度,
 	IS_NULLABLE 是否为空,
 	COLUMN_DEFAULT 默认值,
@@ -35,4 +34,5 @@ FROM
 WHERE
 	table_schema = 'supermarket' 
 	AND table_name = 'supermarket'
+ORDER BY ORDINAL_POSITION ASC
 ```
--- cluster/mysql-cluster/mc/app-sql/supermarket/超市商品数据库应用.sql
+++ cluster/mysql-cluster/mc/app-sql/supermarket/超市商品数据库应用.sql
@@ -1,26 +1,29 @@
 CREATE DATABASE IF NOT EXISTS `supermarket`;
 USE `supermarket`;
 
-DROP TABLE IF EXISTS `supermarket`;
-CREATE TABLE `supermarket` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名称',
-  `address` varchar(255) NOT NULL DEFAULT '' COMMENT '地址',
+  `price` int(10) NOT NULL DEFAULT '0' COMMENT '价格:单位（分）',
+  `goods_category_id` int(10) NOT NULL DEFAULT '0' COMMENT '商品分类表主键',
   `creator` varchar(32) NOT NULL DEFAULT '' COMMENT '创建者',
   `updater` varchar(32) NOT NULL DEFAULT '' COMMENT '更新者',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=NDBCLUSTER DEFAULT CHARSET=utf8mb4 COMMENT='超市表';
+) ENGINE=NDBCLUSTER DEFAULT CHARSET=utf8mb4 COMMENT='商品表';
 
-INSERT INTO `supermarket`.`supermarket` (`id`, `name`, `address`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (1, '超市1', '超市1地址', '管理员', '管理员', '2024-05-04 00:20:15', '2024-05-04 00:20:15');
-INSERT INTO `supermarket`.`supermarket` (`id`, `name`, `address`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (2, '超市2', '超市2地址', '管理员', '管理员', '2024-05-04 00:20:46', '2024-05-04 00:20:46');
-INSERT INTO `supermarket`.`supermarket` (`id`, `name`, `address`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (3, '超市3', '超市3地址', '管理员', '管理员', '2024-05-04 00:20:53', '2024-05-04 00:20:53');
+INSERT INTO `supermarket`.`goods` (`id`, `name`, `price`, `goods_category_id`,  `creator`, `updater`, `created_at`, `updated_at`) VALUES (1, '鸿星尔克鞋子', 10000, 1, '管理员', '管理员', '2024-05-04 00:16:43', '2024-05-04 00:16:43');
+INSERT INTO `supermarket`.`goods` (`id`, `name`, `price`, `goods_category_id`,  `creator`, `updater`, `created_at`, `updated_at`) VALUES (2, '耐克鞋', 20000, 2, '管理员', '管理员', '2024-05-04 00:16:52', '2024-05-04 00:16:52');
+INSERT INTO `supermarket`.`goods` (`id`, `name`, `price`, `goods_category_id`,  `creator`, `updater`, `created_at`, `updated_at`) VALUES (3, '乐事薯片', 1000, 3, '管理员', '管理员', '2024-05-04 00:17:18', '2024-05-04 00:17:18');
+INSERT INTO `supermarket`.`goods` (`id`, `name`, `price`, `goods_category_id`,  `creator`, `updater`, `created_at`, `updated_at`) VALUES (4, '海飞丝洗发水', 2000, 4, '管理员', '管理员', '2024-05-04 00:17:37', '2024-05-04 00:17:37');
+
 
 DROP TABLE IF EXISTS `goods_category`;
 CREATE TABLE `goods_category` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名称',
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '分类名称',
   `creator` varchar(32) NOT NULL DEFAULT '' COMMENT '创建者',
   `updater` varchar(32) NOT NULL DEFAULT '' COMMENT '更新者',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -33,28 +36,9 @@ INSERT INTO `supermarket`.`goods_category` (`id`, `name`, `creator`, `updater`, 
 INSERT INTO `supermarket`.`goods_category` (`id`, `name`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (3, '食品', '管理员', '管理员', '2024-05-04 00:15:38', '2024-05-04 00:15:38');
 INSERT INTO `supermarket`.`goods_category` (`id`, `name`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (4, '洗漱用品', '管理员', '管理员', '2024-05-04 00:15:49', '2024-05-04 00:15:49');
 
-DROP TABLE IF EXISTS `goods`;
-CREATE TABLE `goods` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名称',
-  `price` int(10) NOT NULL DEFAULT '0' COMMENT '价格:单位（分）',
-  `goods_category_ids` varchar(255) NOT NULL DEFAULT '' COMMENT '商品分类ids,多个逗号分隔',
-  `supermarket_id` int(10) NOT NULL DEFAULT '0' COMMENT '超市id',
-  `creator` varchar(32) NOT NULL DEFAULT '' COMMENT '创建者',
-  `updater` varchar(32) NOT NULL DEFAULT '' COMMENT '更新者',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`)
-) ENGINE=NDBCLUSTER DEFAULT CHARSET=utf8mb4 COMMENT='商品表';
-
-INSERT INTO `supermarket`.`goods` (`id`, `name`, `price`, `goods_category_ids`, `supermarket_id`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (1, '鸿星尔克鞋子', 10000, '1', 3, '管理员', '管理员', '2024-05-04 00:16:43', '2024-05-04 00:16:43');
-INSERT INTO `supermarket`.`goods` (`id`, `name`, `price`, `goods_category_ids`, `supermarket_id`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (2, '耐克鞋', 20000, '1', 1, '管理员', '管理员', '2024-05-04 00:16:52', '2024-05-04 00:16:52');
-INSERT INTO `supermarket`.`goods` (`id`, `name`, `price`, `goods_category_ids`, `supermarket_id`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (3, '乐事薯片', 1000, '2', 2, '管理员', '管理员', '2024-05-04 00:17:18', '2024-05-04 00:17:18');
-INSERT INTO `supermarket`.`goods` (`id`, `name`, `price`, `goods_category_ids`, `supermarket_id`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (4, '海飞丝洗发水', 2000, '3', 1, '管理员', '管理员', '2024-05-04 00:17:37', '2024-05-04 00:17:37');
-
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(255) NOT NULL DEFAULT '' COMMENT '姓名',
   `username` varchar(255) NOT NULL DEFAULT '' COMMENT '账号',
   `password` varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
@@ -71,19 +55,22 @@ INSERT INTO `supermarket`.`user` (`id`, `name`, `username`, `password`, `creator
 
 DROP TABLE IF EXISTS `goods_orders`;
 CREATE TABLE `goods_orders` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '用户id',
-  `goods_id` int(10) NOT NULL DEFAULT '0' COMMENT '商品id',
-  `supermarket` int(10) NOT NULL DEFAULT '0' COMMENT '超市id',
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `sn` varchar(255) NOT NULL DEFAULT '' COMMENT '订单编号',
+  `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '用户表主键',
+  `goods_id` int(10) NOT NULL DEFAULT '0' COMMENT '商品表主键',
   `price` int(10) NOT NULL DEFAULT '0' COMMENT '价格,单位:分',
+  `num` int(10) NOT NULL DEFAULT '0' COMMENT '数量',
+  `buy_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '采购时间',
   `creator` varchar(32) NOT NULL DEFAULT '' COMMENT '创建者',
   `updater` varchar(32) NOT NULL DEFAULT '' COMMENT '更新者',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
+  KEY `sn` (`sn`),
   KEY `user_id` (`user_id`),
   KEY `goods_id` (`goods_id`)
 ) ENGINE=NDBCLUSTER DEFAULT CHARSET=utf8mb4 COMMENT='商品订单表';
 
-INSERT INTO `supermarket`.`goods_orders` (`id`, `user_id`, `goods_id`, `supermarket`, `price`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (1, 1, 1, 1, 10000, '管理员', '管理员', '2024-05-04 00:25:41', '2024-05-04 00:25:41');
-INSERT INTO `supermarket`.`goods_orders` (`id`, `user_id`, `goods_id`, `supermarket`, `price`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (2, 2, 2, 2, 20000, '管理员', '管理员', '2024-05-04 00:25:49', '2024-05-04 00:25:49');
+INSERT INTO `supermarket`.`goods_orders` (`id`, `sn`, `user_id`, `goods_id`, `price`, `num`, `buy_at`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (1, '1715062603', 1, 1, 10000, 5, '2024-05-06 12:25:49', '管理员', '管理员', '2024-05-04 00:25:41', '2024-05-04 00:25:41');
+INSERT INTO `supermarket`.`goods_orders` (`id`, `sn`, `user_id`, `goods_id`, `price`, `num`, `buy_at`, `creator`, `updater`, `created_at`, `updated_at`) VALUES (2, '1715062604', 2, 2, 20000, 6, '2024-05-06 12:25:49', '管理员', '管理员', '2024-05-04 00:25:49', '2024-05-04 00:25:49');
